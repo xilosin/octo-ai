426fffc1ffc94a1316c480be3cfacfa544107358
Farhan Khorsheed <ratul097@gmail.com>
2023-07-27 01:35:54 +1000

    [ADDED] Navbar, Sidebar and Dashboard components

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,93 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { 
+	ArrowRight, 
+	Code,
+	ImageIcon,
+	MessageSquare,
+	Music,
+	VideoIcon
+} from 'lucide-react'
+
+import { Card } from '@/components/ui/card'
+import { cn } from '@/lib/utils';
+
+const tools = [
+	{
+		label: 'Conversation',
+		icon: MessageSquare,
+		color: 'text-blue-400',
+		bgColor: 'bg-blue-400/10',
+		href: '/converation'
+	},
+	{
+		label: 'Image Generation',
+		icon: ImageIcon,
+		color: 'text-pink-400',
+		bgColor: 'bg-pink-400/10',
+		href: '/image'
+	},
+	{
+		label: 'Video Generation',
+		icon: VideoIcon,
+		color: 'text-yellow-400',
+		bgColor: 'bg-yellow-400/10',
+		href: '/video'
+	},
+	{
+		label: 'Music Generation',
+		icon: Music,
+		color: 'text-green-400',
+		bgColor: 'bg-green-400/10',
+		href: '/music'
+	},
+	{
+		label: 'Code Generation',
+		icon: Code,
+		color: 'text-orange-400',
+		bgColor: 'bg-orange-400/10',
+		href: '/code'
+	},
+]
+
 const DashboardPage = () => {
-  return <p>DashboardPage (Protected)</p>;
+	const router = useRouter()
+
+	return (
+		<div>
+			<div className="mb-8 space-y-4">
+				<h2 className="text-2xl md:text-4xl font-bold text-center">
+					Explore the power of AI
+				</h2>
+				<p className="text-muted-foreground font-light text-sm 
+					md:text-lg text-center"
+				>
+					Chat with the smartest AI - Experience the power of AI
+				</p>
+			</div>
+			<div className="px-4 md:px-20 lg:px-32 space-y-4">
+				{tools.map((tool) => (
+					<Card
+						onClick={() => router.push(tool.href)}
+						key={tool.href}
+						className='p-4 border-black/5 flex items-center 
+							justify-between hover:shadow-md transition cursor-pointer'
+					>
+						<div className='flex items-center gap-x-4'>
+							<div className={cn(`p-2 w-fit rounded-md`, tool.bgColor)}>
+								<tool.icon className={cn(`w-8 h-8`, tool.color)} />
+							</div>
+							<div className='font-semibold'>
+								{tool.label}
+							</div>
+						</div>
+						<ArrowRight className='w-5 h-5' />
+					</Card>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default DashboardPage;
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,21 @@
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+
 const LandingPage = () => {
-  return <div>LandingPage (Unprotected)</div>;
+  return (
+    <div>
+      LandingPage (Unprotected)
+      <div>
+        <Link href="/sign-in">
+          <Button>Sign in</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Sign up</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
