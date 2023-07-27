007220f3f2bbd43e0d143377f4d00b9fcdc7da3b
Farhan Khorsheed <ratul097@gmail.com>
2023-07-28 06:02:23 +1000

    [UPDATED] Removed unused icon import

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { Menu, X } from 'lucide-react';
+import { Menu } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import {
